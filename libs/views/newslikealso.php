96a127b10c0c2ae1245b75736721c166143857bc
Minh Dang <minhdangthanh@hotmail.com>
2014-11-13 18:28:22 +0700

    continue 13112014

---
+++ libs/views/newslikealso.php
@@ -0,0 +1,88 @@
+<?php 
+    if(isset($_SESSION['like-also']))
+    {
+        $listNewsId = substr($_SESSION['like-also'], 0, -1);
+    }
+    
+	$modNews = new mod_news();
+	$alsoLike = $modNews->GetNewsAlsoLike($contentTitle, $contentDesc, $listNewsId);
+	
+	if(count($alsoLike) > 0)
+	{
+		if(count($alsoLike) > 4)
+		{
+			$arr = array_rand($alsoLike, 4);
+		}
+		else
+		{
+			$arr = $alsoLike;
+		}
+?>
+<div class="also-like">
+	<div class="also-like-title">
+		you may also like :
+	</div>
+	<div class="also-like-main">
+	<?php 
+	$clsCommon = new SBD_Common();
+	
+	$modCategories = new mod_categories();
+	
+	foreach ($arr as $newsItem)
+	{
+		if($newsItem['new_id'] != $id)
+		{
+		$currCategory = $modCategories->GetCategory($newsItem['new_cat_id']);
+			
+		$pathCat = $clsCommon->text_rewrite($currCategory[0]['cat_name']) . '-' . $currCategory[0]['cat_id'];
+			
+		$link = BASE_NAME . 'news/' . $pathCat . '/' . $clsCommon->text_rewrite($newsItem['new_title']) . '-' . $newsItem['new_id'] . '/';
+	?>
+		<article class="also-like-article">
+			<section class="also-like-box">
+				<div class="also-like-inner-box">
+					<a href="<?php echo $link;?>">
+		    			<img class="also-like-img" alt="my offer" src="<?php echo BASE_NAME . 'uploads/' . $newsItem['new_img_path'] ;?>" />
+		    		</a>
+				</div>
+			</section>
+			<section class="also-like-line">
+				<div>
+    				<img alt="NAM PLUS" src="<?php echo BASE_NAME;?>images/nam-icon-small.png" />
+				</div>	
+			</section>
+			<section class="also-like-box">
+				<div class="also-like-inner-box">
+					<div class="display-table">
+						<div class="display-table-cell">
+							<div class="div-new-content">
+								<div class="title-new-cat"><?php echo $currCategory[0]['cat_name'];?></div>
+								<h3 class="title-new-content">
+									<?php echo $newsItem['new_title'];?>
+								</h3>
+								<div class="title-new-context">
+									<?php									
+									echo $newsItem['new_description'];
+									?>
+								</div>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</article>
+	<?php 
+		}
+	}
+	?>
+	</div>
+</div>
+<div style="clear: both; height: 1px;">
+		&nbsp;
+</div>
+<?php 
+
+	}
+
+?>
